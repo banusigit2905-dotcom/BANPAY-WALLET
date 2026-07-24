3c60f3788509e32facddb263b4dff7faa844e2a2
banusigit2905-dotcom <banusigit2905@gmail.com>
2026-07-24 21:03:17 +0700

    Refactor Firebase configuration and initialization
    Updated Firebase configuration to use new project settings and SDK imports.

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // KONFIGURASI FIREBASE ANDA
-const firebaseConfig = {
-    apiKey: "AIzaSy...",
-    authDomain: "proyek-anda.firebaseapp.com",
-    projectId: "proyek-anda",
-    storageBucket: "proyek-anda.appspot.com",
-    messagingSenderId: "12345",
-    appId: "1:12345:web:abcde"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyASM8OChiMNBZo_vwATol0xbKkB80XBm2M",
+    authDomain: "banpay-wallet.firebaseapp.com",
+    projectId: "banpay-wallet",
+    storageBucket: "banpay-wallet.firebasestorage.app",
+    messagingSenderId: "481060026912",
+    appId: "1:481060026912:web:32bfa9b8677e77bcee1a45",
+    measurementId: "G-P4H64D5M9T"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 // Inisialisasi
 firebase.initializeApp(firebaseConfig);
